490e583b3ea1d56a646c7354154900f58d79906a
Aditi-Patro <aditi.patro01@gmail.com>
2025-05-19 10:24:48 +0530

    Python Assessment No 2

---
+++ Topic2 task4.py
@@ -0,0 +1,15 @@
+#Experiment with different arithmetic and logical operators in the interpreter.
+
+#Arithmetic Operators
+print("Addition: ",5+3)
+print("Subtraction: ",10-3)
+print("Multiplication: ",2*4)
+print("Division: ",10/4)
+print("Modulus: ",20%3)
+print("Exponentiation: ",3**2)
+print("Floor Division: ",45//3)
+
+#Logical Operators
+print("and operator: ",6<4 and 1<10)
+print("or operator: ",6<4 or 1<10)
+print("not operator: ",not(6<4 and 1<10))
