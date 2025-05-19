c7faa7a32e5a7b801613c852fd1168aca64b0c6a
Aditi-Patro <aditi.patro01@gmail.com>
2025-05-19 10:15:09 +0530

    Python Assessment No 2

--- Topic2 task2.py
+++ Topic2 task2.py
@@ -1,3 +1,4 @@
+#Write a script that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
 n1=int(input("Enter 1st number:"))
 n2=int(input("Enter 2nd number:"))
 if n1>n2:
@@ -5,4 +6,4 @@ if n1>n2:
 elif n1<n2:
     print(str(n1)+" is less than "+str(n2))
 elif n1==n2:
-    print(str(n1)+" is equal to "+str(n2)) 
+    print(str(n1)+" is equal to "+str(n2))
