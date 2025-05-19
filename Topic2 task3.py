076094b6b71a7a3754e337e76cea28c62e9097e6
Aditi-Patro <aditi.patro01@gmail.com>
2025-05-19 10:15:43 +0530

    Python Assessment No 2

--- Topic2 task3.py
+++ Topic2 task3.py
@@ -1,3 +1,4 @@
+#Write a script that checks if a given year is a leap year (divisible by 4, but not by 100 unless also divisible by 400). 
 year=int(input("Enter the year:"))
 if (year%4==0 and year%100!=0) or (year%400==0):
     print(str(year)+" is a leap year")
